7d4ef30e1f1f68aeb697318360acf172cde0e4ea
franjavi96a <franjavi.alv96@live.com>
2025-03-15 15:07:51 -0500

    feature/pantalla de ingreso Login funcional y terminada

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-    return (
-        <div>
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(import.meta.env.VITE_API_URL + '/login', { username, password });
+            localStorage.setItem('token', response.data.token);
+            //Aqui se podria decodificar el token y obtener el rol y redirecionar a la pagina correspondiente
+            console.log(response.data);
+            navigate('/dashboard');
+        } catch (error) {
+            alert('Credenciales incorrectas');
+            console.log(error);
+        }
+    }
+
+    return (
+        <div className='container mt-5'>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label className='form-label'>Username</label>
+                    <input
+                        type='text'
+                        className='form-control'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)} required
+                    />
+                </div>
+                <div className='mb-3'>
+                    <label className='form-label'>Password</label>
+                    <input
+                        type='password'
+                        className='form-control'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} required
+                    />
+
+                </div>
+                <button type='submit' className='btn btn-primary'>Login</button>
+            </form>
         </div>
     )
 }
